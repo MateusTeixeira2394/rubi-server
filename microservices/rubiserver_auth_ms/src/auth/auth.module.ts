4d663c62ea09734b3c7b31c3e7105d6251108bd0
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2024-04-14 21:04:38 -0300

    Added the configModule to the project

--- microservices/rubiserver_auth_ms/src/auth/auth.module.ts
+++ microservices/rubiserver_auth_ms/src/auth/auth.module.ts
@@ -3,12 +3,14 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { UsersModule } from "src/users/users.module";
 import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
     UsersModule,
+    ConfigModule.forRoot(),
     JwtModule.register({
-      secret: "this_secret_must_be_imported_from_environment_variable",
+      secret: process.env.ACCESS_TOKEN_ENCRYPTION_KEY,
     }),
   ],
   controllers: [AuthController],
